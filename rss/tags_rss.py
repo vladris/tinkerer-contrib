670823cd6f2f4effa1b242505679b7f29ed590d7
Atamert Ölçgen <muhuk@muhuk.com>
2014-07-13 16:31:02 +0000

    Change the suffix to .xml for generated RSS feeds.

--- rss/tags_rss.py
+++ rss/tags_rss.py
@@ -10,27 +10,39 @@
     :copyright: Copyright 2013 by Atamert Ölçgen
     :license: BSD license
 '''
+import os
 import rss
 from tinkerer import utils
-
-
-def html_collect_pages(app):
-    '''
-    Collect html pages and emit event
-    '''
-    for name, context, template in generate_feeds(app):
-        yield (name, context, template)
 
 
 def generate_feeds(app):
     env = app.builder.env
     categories = env.filing["categories"]
 
-    for category_name, posts in env.filing["tags"].items():
-        for name, context, template in rss.generate_feed(app, category_name, posts):
-            slug = utils.name_from_title(name)
-            yield ("rss/tags/%s" % slug, context, template)
+    for tag_name, posts in env.filing["tags"].items():
+        app.info("Generating RSS feed for tag '%s'." % tag_name)
+        name, context, template = rss.generate_feed(app, tag_name, posts)
+        slug = utils.name_from_title(name)
+        yield ("rss/tags/%s" % slug, context, template)
+
+
+def on_html_collect_pages(app):
+    '''
+    Collect html pages and emit event
+    '''
+    for name, context, template in generate_feeds(app):
+        yield (name, context, template)
+        app.emit("rss-tag-page-generated", name)
+
+
+def on_rss_tag_page_generated(app, name):
+    src_file = "%s/%s.html" % (app.outdir, name)
+    dest_file = "%s/%s.xml" % (app.outdir, name)
+    app.info("Renaming: '%s' as '%s'." % (src_file, dest_file))
+    os.rename(src_file, dest_file)
 
 
 def setup(app):
-    app.connect("html-collect-pages", html_collect_pages)
+    app.add_event("rss-tag-page-generated")
+    app.connect("html-collect-pages", on_html_collect_pages)
+    app.connect("rss-tag-page-generated", on_rss_tag_page_generated)
